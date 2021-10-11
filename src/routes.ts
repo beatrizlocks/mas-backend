d425f9c47076a4fdc64b26babe2ade0ec2f1ebbf
beatrizlocks <82235826+beatrizlocks@users.noreply.github.com>
2021-10-11 20:15:55 -0300

    Chore: models, controller,service e migration

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,21 @@
-import { Router,response } from "express";
+import { Router, Request, Response, response } from "express";
+import {UserController} from './controller/UserController';
 
+interface UserRequest{
+    name:string;
+    email:string;
+    password:string;
+}
+
+const userController = new UserController()
 const routes = Router();
+
+routes.post('/user',() => console.log('User route'));
+routes.post('/activy',() => console.log('Activy route'));
+routes.post('/courseunit',() => console.log('courseunit route'));
 
 routes.get('/user', (request, response) => response.json({
     message:"hello world"
 }))
-
-
 
 export default routes;
